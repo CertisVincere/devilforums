a77fb69e7c58c20ea7c66c7862c8737d1cce4ebe
Jack Li <jackprius@gmail.com>
2016-03-18 10:50:31 -0700

    Remove elasticsearch

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,13 +1,8 @@
-require 'elasticsearch/model'
-
 class Group < ActiveRecord::Base
   has_many :users
   has_many :topics, :dependent => :destroy
 
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
   validates :category, presence: true, length: { maximum: 20 }
-
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
 
 end
